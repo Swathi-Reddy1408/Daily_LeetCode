c3a614a06b71718e976a2bd5afb4731c9ae24b94
Swathi Thokala <swathireddythokala16@gmail.com>
2025-01-02 16:26:42 -0600

    Jan 01 soln

--- December_2024/12_31_2024/Solution_LeetCode_983.py
+++ December_2024/12_31_2024/Solution_LeetCode_983.py
@@ -4,14 +4,15 @@ class Solution:
         def dfs(curDay):
             if curDay > days[-1]:
                 return 0
+            #Memoizing the results
             if dp[curDay] != -1:
                 return dp[curDay]
+            #Branching into three possibilities
             if curDay in days:
                 dp[curDay] = min(costs[0] + dfs(curDay + 1), costs[1] + dfs(curDay + 7), costs[2] + dfs(curDay + 30))
             else:
                 dp[curDay] = dfs(curDay + 1)
             return dp[curDay]
-        print(dfs(0))
         return dfs(0)
 
         
